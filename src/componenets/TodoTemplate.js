86d5d7572dc69bbc86f41d984ab781d581d5fd78
wooyong99 <wulovesk@naver.com>
2024-02-23 11:51:14 +0900

    feat : Text 컴포넌트

--- src/componenets/TodoTemplate.js
+++ src/componenets/TodoTemplate.js
@@ -1,13 +1,15 @@
 import './TodoTemplate.scss';
 import React from 'react';
 import { MdCalendarToday } from 'react-icons/md';
-
+import Text from './Text';
 const TodoTemplate = ({ children }) => {
   return (
     <div className="TodoTemplate">
       <div className="app-title">
         <MdCalendarToday />
-        <h>일정관리</h>
+        <Text type="h5" color="white">
+          일정관리
+        </Text>
       </div>
       <div className="content">{children}</div>
     </div>
